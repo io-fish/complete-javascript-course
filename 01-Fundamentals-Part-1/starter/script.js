1992559c5d9d8556e65da3a6ccc75626fc1d7b14
Ioannis Psaronikolakis <124346612+ipsaronikolakis@users.noreply.github.com>
2024-03-13 05:58:53 +0200

    Finished fundamentals

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -32,31 +32,42 @@
 // // "Jonas has 3 friends, and his best friend is called Michael"
 // console.log(`${jonas.firstName} has ${jonas.friends.length} friends, and his best friend is called ${jonas.friends[0]}`)
 
-const jonas = {
-    firstName: 'Jonas',
-    lastName: 'Schmedtmann',
-    birthYear: 1991,
-    job: 'teacher',
-    friends: ['Michael', 'Peter', 'Steven'],
-    hasDriversLicense: true,
+// const jonas = {
+//     firstName: 'Jonas',
+//     lastName: 'Schmedtmann',
+//     birthYear: 1991,
+//     job: 'teacher',
+//     friends: ['Michael', 'Peter', 'Steven'],
+//     hasDriversLicense: true,
 
-    calcAge: function () {
-        return 2037 - this.birthYear;
-    },
-    getSummary: function () {
-        return `${this.firstName} is a ${this.calcAge()}-year old ${jonas.job}, and he has ${this.hasDriversLicense ? 'a' : 'no'} driver's license.`
-    }
+//     calcAge: function () {
+//         return 2037 - this.birthYear;
+//     },
+//     getSummary: function () {
+//         return `${this.firstName} is a ${this.calcAge()}-year old ${jonas.job}, and he has ${this.hasDriversLicense ? 'a' : 'no'} driver's license.`
+//     }
 
-};
+// };
 
-console.log(jonas.calcAge());
+// console.log(jonas.calcAge());
 
-const age = jonas.calcAge();
-console.log(age);
+// const age = jonas.calcAge();
+// console.log(age);
 
-// "jonas is a 46-year old teacher, and he has a drivers license"
-console.log(jonas.getSummary());
+// // "jonas is a 46-year old teacher, and he has a drivers license"
+// console.log(jonas.getSummary());
 
-for (let i = 0; i < jonas.friends.length; i++) {
-    console.log(jonas.friends[i]);
-}
+// for (let i = 0; i < jonas.friends.length; i++) {
+//     console.log(jonas.friends[i]);
+// }
+
+// for (let i = 0; i < jonas.friends.length; i++) {
+//     console.log(jonas['friends'][i]);
+// }
+
+// for (let exercise = 1; exercise <= 3; exercise++) {
+//     console.log(`------Start exercise ${exercise}------`);
+//     for (let rep = 1; rep <= 3; rep++) {
+//         console.log(`Lifting weights repetition ${rep} 🏋️‍♀️`);
+//     }
+// }
