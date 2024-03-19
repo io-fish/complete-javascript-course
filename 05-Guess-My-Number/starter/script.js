b7c959bb8b24e57a10c466737a59f46ab0afaa79
Ioannis Psaronikolakis <124346612+ipsaronikolakis@users.noreply.github.com>
2024-03-19 09:33:01 +0200

    Completed secret number implementation

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -19,35 +19,73 @@ console.log(document.querySelector('.guess').value);
 //     document.querySelector('.message').textContent = message;
 // };
 
-const secretNumber = Math.trunc(Math.random() * 20) + 1;
-let score = 20;
+const displayMessage = function (message) {
+    document.querySelector('.message').textContent = message;
+}
+const secretNumber = function () {
+    Math.trunc(Math.random() * 20) + 1;
+}
 
-document.querySelector('.number').textContent = secretNumber
+let score = 20;
+let highscore = 0;
+
 document.querySelector('.check').addEventListener('click', function () {
     const guess = Number(document.querySelector('.guess').value);
     console.log(guess, typeof guess);
 
     if (!guess) {
-        document.querySelector('.message').textContent = '⛔ No number!';
+        displayMessage('⛔ No number!');
     } else if (guess === secretNumber) {
-        document.querySelector('.message').textContent = '🎉 Correct Number!';
-    } else if (guess > secretNumber) {
-        if (score > 1) {
-            document.querySelector('.message').textContent = 'Too high!';
-            score--;
-            document.querySelector('.score').textContent = score;
-        } else {
-            document.querySelector('.message').textContent = '💥 You lost the game!';
-            document.querySelector('.score').textContent = 0;
+        displayMessage('🎉 Correct Number!');
+        document.querySelector('body').style.backgroundColor = '#60b347';
+        document.querySelector('.number').style.width = '30rem';
+        document.querySelector('.number').textContent = secretNumber
+
+        if (score > highscore) {
+            highscore = score;
+            document.querySelector('.highscore').textContent = highscore;
         }
-    } else if (guess < secretNumber) {
+    } else if (guess !== secretNumber) {
         if (score > 1) {
-            document.querySelector('.message').textContent = 'Too low!';
+            displayMessage(guess > secretNumber ? '📈 Too high!' : '📉 Too low!');
             score--;
             document.querySelector('.score').textContent = score;
         } else {
-            document.querySelector('.message').textContent = '💥 You lost the game!';
+            displayMessage('💥 You lost the game!');
             document.querySelector('.score').textContent = 0;
         }
     }
+
+    // else if (guess > secretNumber) {
+    //     if (score > 1) {
+    //         document.querySelector('.message').textContent = 'Too high!';
+    //         score--;
+    //         document.querySelector('.score').textContent = score;
+    //     } else {
+    //         document.querySelector('.message').textContent = '💥 You lost the game!';
+    //         document.querySelector('.score').textContent = 0;
+    //     }
+    // } else if (guess < secretNumber) {
+    //     if (score > 1) {
+    //         document.querySelector('.message').textContent = 'Too low!';
+    //         score--;
+    //         document.querySelector('.score').textContent = score;
+    //     } else {
+    //         document.querySelector('.message').textContent = '💥 You lost the game!';
+    //         document.querySelector('.score').textContent = 0;
+    //     }
+    // }
+})
+
+document.querySelector('.again').addEventListener('click', function () {
+    secretNumber();
+    score = 20;
+    // document.querySelector('.message').textContent = 'Start guessing...';
+    displayMessage('Start guessing...');
+    document.querySelector('.highscore').textContent = highscore;
+    document.querySelector('.score').textContent = score;
+    document.querySelector('.number').textContent = '?';
+    document.querySelector('.guess').value = '';
+    document.querySelector('body').style.backgroundColor = '#222';
+    document.querySelector('.number').style.width = '15rem';
 })
