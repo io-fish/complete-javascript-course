62856e2564a761bf1e8e8152535a9894c0000188
Ioannis Psaronikolakis <124346612+ipsaronikolakis@users.noreply.github.com>
2024-05-08 20:30:51 +0300

    Hoisting and this theory

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,9 +1,105 @@
 'use strict';
 
+// function calcAge(birthYear) {
+//     const age = 2037 - birthYear;
+//     console.log(firstName);
+// }
+
+// const firstName = 'Jonas';
+// calcAge(1991);
+
+
+///////////////////////////////////////
+// Scoping in Practice
+
+/*
 function calcAge(birthYear) {
-    const age = 2037 - birthYear;
-    console.log(firstName);
+  const age = 2037 - birthYear;
+
+  function printAge() {
+    let output = `${firstName}, you are ${age}, born in ${birthYear}`;
+    console.log(output);
+
+    if (birthYear >= 1981 && birthYear <= 1996) {
+      var millenial = true;
+      // Creating NEW variable with same name as outer scope's variable
+      const firstName = 'Steven';
+
+      // Reasssigning outer scope's variable
+      output = 'NEW OUTPUT!';
+
+      const str = `Oh, and you're a millenial, ${firstName}`;
+      console.log(str);
+
+      function add(a, b) {
+        return a + b;
+      }
+    }
+    // console.log(str);
+    console.log(millenial);
+    // console.log(add(2, 3));
+    console.log(output);
+  }
+  printAge();
+
+  return age;
 }
 
 const firstName = 'Jonas';
 calcAge(1991);
+// console.log(age);
+// printAge();
+
+
+///////////////////////////////////////
+// Hoisting and TDZ in Practice
+
+// Variables
+console.log(me);
+// console.log(job);
+// console.log(year);
+
+var me = 'Jonas';
+let job = 'teacher';
+const year = 1991;
+
+// Functions
+console.log(addDecl(2, 3));
+// console.log(addExpr(2, 3));
+console.log(addArrow);
+// console.log(addArrow(2, 3));
+
+function addDecl(a, b) {
+  return a + b;
+}
+
+const addExpr = function (a, b) {
+  return a + b;
+};
+
+var addArrow = (a, b) => a + b;
+*/
+
+// Example
+// if (!numProducts) deleteShoppingItem();
+
+// var numProducts = 10;
+
+// function deleteShoppingItem() {
+//     console.log('All products deleted!')
+// }
+
+// console.log(a)
+// var a = 1;
+// a = 2;
+// console.log(a)
+
+const jonas = {
+  name: 'Jonas',
+  year: 1989,
+  calcAge: function () {
+    console.log(2037 - this.year);
+  }
+}
+
+jonas.calcAge()
